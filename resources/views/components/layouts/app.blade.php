ee043726a2c395268a71c986a4023d21f8f56b3c
Josef Bauer <josef.bauer.1st@gmail.com>
2025-02-13 21:56:51 +0100

    Add TailwindCSS 4 and daisyUI

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Zonelist - Bestsellers of Amazone</title>
+    <title>Zonelist - Amazone Bestsellers</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
